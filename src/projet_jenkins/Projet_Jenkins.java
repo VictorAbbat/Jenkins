e1de10e103801ab8380fb74eeb23eb3565f9623b
Fred <2095041@bdeb.qc.ca>
2020-11-27 15:45:09 -0500

    Modification du sout - Victor

--- src/projet_jenkins/Projet_Jenkins.java
+++ src/projet_jenkins/Projet_Jenkins.java
@@ -16,6 +16,7 @@ public class Projet_Jenkins {
      */
     public static void main(String[] args) {
         System.out.println("Bonjour "+args[1]+", " +args[0]);
+        System.out.println("Merci "+args[1]+", " +args[0]);
     }
     
 }
